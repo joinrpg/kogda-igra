e715c6d85d356cade88db292e21154c87194d71e
Leonid Tsarev <leo@bastilia.ru>
2012-02-14 17:05:00 +0400

    Игры-пересечения сортируются по количеству игроков (крупные сверху)

--- public_html/appcode/logic/gameinfo.php
+++ public_html/appcode/logic/gameinfo.php
@@ -10,7 +10,9 @@ function get_intersections($game_id)
 			AND kgd2.game_id = $game_id AND kgd.`order` = 0 AND kgd2.`order` = 0", "
 			INNER JOIN ki_game_date kgd2 ON (kgd.begin <= kgd2.begin + INTERVAL kgd2.time DAY AND kgd.begin >= kgd2.begin)
         OR (kgd2.begin <= kgd.begin + INTERVAL kgd.time DAY AND kgd2.begin >= kgd.begin)
-			 ");
+			 ",
+			 '',
+			 "kg.players_count DESC, kgd.begin DESC, kgd.time");
 }
 
 ?>
