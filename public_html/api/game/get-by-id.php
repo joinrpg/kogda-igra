5f0930987d2c8b964295421f28cd6b463de626d4
Leonid Tsarev <leo@bastilia.ru>
2013-10-22 23:47:05 +0400

    Очистка объекта «игра» в API в отдельную функцию

--- public_html/api/game/get-by-id.php
+++ public_html/api/game/get-by-id.php
@@ -2,37 +2,13 @@
 <?php
 	require_once 'funcs.php';
 	require_once 'logic/gamebase.php';
+	require_once 'api.php';
 
 	$id = array_key_exists('id', $_GET) ? intval($_GET['id']) : '';
   $result = get_game_by_id($id);
   if (is_array($result))
-  {
-    if ($result['show_flags'])
-    {
-      echo "{\"id\":\"$id\", \"access-denied\":\"1\"}";die();
-    }
-    if ($result['redirect_id'])
-    {
-      $redirect_id = $result['redirect_id'];
-      echo "{\"id\":\"$id\", \"redirect_id\":\"$redirect_id\"}";die();
-    }
-    if ($result['hide_email'])
-    {
-      unset($result['email']);
-    }
-    $enabled_fields = 
-      array ("id", "name", "uri", "begin", "time", "type", "polygon", "mg", "email", "status", "comment", "region", "sub_region_id", "deleted_flag", 
-      "players_count", "allrpg_info_id", "polygon_name", "game_type_name", "sub_region_disp_name", "sub_region_name", "status_name");
-    $response = array();
-    foreach ($result as $key => $value)
-    {
-      if (array_search($key, $enabled_fields))
-      {
-        $response[$key] = $result[$key];
-      }
-    }
-    
-    echo json_encode($response);
+  { 
+    echo json_encode(strip_game_object_before_json($result));
   }
   else
   {
---
+++ public_html/appcode/api.php
@@ -0,0 +1,35 @@
+<?php
+function strip_game_object_before_json($result)
+{
+    if ($result['show_flags'])
+    {
+      return array(
+        'id' => $result['id'],
+        'access-denied' => 1,
+        );
+    }
+    if ($result['redirect_id'])
+    {
+      return array(
+        'id' => $result['id'],
+        'redirect_id' => $result['redirect_id'],
+        );
+    }
+    if ($result['hide_email'])
+    {
+      unset($result['email']);
+    }
+    $enabled_fields = 
+      array ("id", "name", "uri", "begin", "time", "type", "polygon", "mg", "email", "status", "comment", "region", "sub_region_id", "deleted_flag", 
+      "players_count", "allrpg_info_id", "polygon_name", "game_type_name", "sub_region_disp_name", "sub_region_name", "status_name");
+    $response = array();
+    foreach ($result as $key => $value)
+    {
+      if (array_search($key, $enabled_fields))
+      {
+        $response[$key] = $result[$key];
+      }
+    }
+    return $response;
+}
+?>
