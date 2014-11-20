eff617b7cc907830d68544afcf23e27286a24130
Leonid Tsarev <leo@bastilia.ru>
2014-11-20 22:47:37 +0300

    Embed VK using https

--- public_html/appcode/funcs.php
+++ public_html/appcode/funcs.php
@@ -26,7 +26,7 @@ function write_header ($title, $edit = FALSE)
 <link rel="search" type="application/opensearchdescription+xml" title="Когда-Игра!" href="http://kogda-igra.ru/opensearch.xml">
 
     <script src="/js/default.js" type="text/javascript"></script>
-    <script type="text/javascript" src="http://userapi.com/js/api/openapi.js?20"></script>
+    <script type="text/javascript" src="https://vk.com/js/api/openapi.js?115"></script>
 <script src="https://login.persona.org/include.js" type="text/javascript"></script>
 <script type="text/javascript">
   VK.init({apiId: 2118784, onlyWidgets: true});
