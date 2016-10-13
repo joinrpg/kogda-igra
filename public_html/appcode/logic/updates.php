30122b572c26ce3bfc8c950414c54164deae880e
Leonid Tsarev <leo@bastilia.ru>
2016-10-13 22:21:27 +0300

    Speed up updates

--- public_html/appcode/logic/updates.php
+++ public_html/appcode/logic/updates.php
@@ -55,8 +55,30 @@ function _get_updates($where, $limit = 0)
 
 function get_updates_for_game($game_id)
 {
-	$game_id = intval ($game_id);
-	return _get_updates("kg.id = $game_id");
+  $game_id = intval ($game_id);
+  $sql = connect();
+  $sql -> Run("SET SQL_BIG_SELECTS=1");
+  return $sql -> Query("
+		SELECT ku.*, kut.ki_update_type_name, kut.update_type_polygon_flag, kut.update_type_game_flag, kut.update_type_photo_flag, kut.update_type_review_flag, kut.update_type_user_text,
+		kg.*, kp.polygon_name, kgt.game_type_name, ksr.sub_region_disp_name, ksr.sub_region_name, kgt.show_all_regions, users.username, ks.status_name, ks.status_style, updated_user.username AS updated_user_name, krev.review_uri, krev.topic_id, krev.show_review_flag, kau.uri, kau.allrpg_info_id
+		FROM ki_updates ku
+		
+		INNER JOIN ki_update_types kut ON ku.ki_update_type_id = kut.ki_update_type_id
+		LEFT JOIN ki_photo kph ON kph.photo_id = ku.photo_id
+		INNER JOIN ki_games kg ON kg.id = ku.game_id
+		LEFT JOIN ki_game_date ON ki_game_date.game_Id = kg.id AND ki_game_date.order = 0 
+		INNER JOIN ki_polygons kp ON kg.polygon = kp.polygon_id
+		LEFT JOIN ki_sub_regions ksr ON kg.sub_region_id = ksr.sub_region_id
+		LEFT JOIN ki_game_types kgt ON kg.type = kgt.game_type_id
+		LEFT JOIN `ki_regions` kr ON ksr.region_id = kr.region_id
+		LEFT JOIN `ki_status` ks ON ks.status_id = kg.status
+		LEFT JOIN `ki_review` krev ON krev.review_id = ku.review_id
+		LEFT JOIN ki_add_uri kau ON kau.add_uri_id = ku.add_uri_id
+		LEFT JOIN `users` updated_user ON updated_user.user_id = ku.updated_user_id OR updated_user.user_id = krev.author_id
+		LEFT JOIN users ON users.user_id = ku.user_id
+		WHERE kg.id = $game_id
+		ORDER BY update_date DESC 
+	");
 }
 
 function get_updates_for_ip($ip)
