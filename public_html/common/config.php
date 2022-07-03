b7875f085de8695f91c10882cd048fbd50df6c47
Leonid Tsarev <leo@leotsarev.ru>
2022-07-03 15:49:29 +0300

    move config.php back to source control

---
+++ public_html/common/config.php
@@ -0,0 +1,15 @@
+<?php
+	$sql_server = 	$_ENV['POSTGRES_SERVER'];
+	$sql_user = 	$_ENV['POSTGRES_USER'];
+	$sql_pass =  	$_ENV['POSTGRES_PASSWORD'];
+	$sql_db =  		$_ENV['POSTGRES_DATABASE'];
+	$sql_port =		$_ENV['POSTGRES_PORT'];
+	
+	define ("SITENAME_EDITORS_EMAIL", 'rpg@kogda-igra.ru');
+	define ("SITENAME_MAIN", "КогдаИгра");
+	define ("SITENAME_SIGNATURE", "Редакторы КогдаИгры");
+	define ("SITENAME_HOST", "kogda-igra.ru");
+	define ("SITENAME_SCHEME", "https");
+	define ("GA_ANALYTICS", "UA-1194519-2");
+	
+?>
