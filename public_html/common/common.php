c5167adb27bbd59a645b9d40c2a7da3ecc699df4
Leonid Tsarev <leo@leotsarev.ru>
2023-07-28 15:24:40 +0300

    extend year to 2030

--- public_html/common/common.php
+++ public_html/common/common.php
@@ -1,7 +1,7 @@
 <?php
 function validate_year ($year)
 {
-	if (settype ($year, 'int') && ($year > 1980) && ($year < 2025))
+	if (settype ($year, 'int') && ($year > 1980) && ($year < 2030))
 	{
 				return $year;
 	}
